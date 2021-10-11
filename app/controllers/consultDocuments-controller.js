5729957cd18ea08ebda23ae8e6b7de124a738d15
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-10-11 13:04:59 -0500

    Modify the name of the attribute where the category arrives to make the query of the lost or found documents

--- app/controllers/consultDocuments-controller.js
+++ app/controllers/consultDocuments-controller.js
@@ -1,7 +1,7 @@
 const db = require("../db/mysql");
 
 let consultDocuments = (req, res) => {
-  db.consultDocuments(req.header)
+  db.consultDocuments(req.query)
     .then((result) => {
       return res.status(200).json({
         status: "Token ok",
