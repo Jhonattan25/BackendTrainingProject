841dd7b570763eb34d9d0d598865a2ea2d0f371d
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-10-12 08:42:26 -0500

    Add the setTimeout in the log query

--- app/controllers/registerUser-controller.js
+++ app/controllers/registerUser-controller.js
@@ -1,11 +1,11 @@
 const db = require("../db/mysql");
 const bcrypt = require("bcryptjs");
 
-let register = (req, res) => {
+let register = async (req, res) => {
   let hashPass = bcrypt.hashSync(req.body.password, 8);
   req.body.password = hashPass;
 
-  db.registerUser(req.body)
+  await db.registerUser(req.body)
   .then((result) => {
     return res.status(200).json({
       status: "Successful registration",
