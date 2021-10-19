f89d901b37c5892467d7176d4e42b2db78d78174
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-10-18 23:09:30 -0500

    The code for sending mail was organized when the user registers

--- app/config/mailer.js
+++ app/config/mailer.js
@@ -1,17 +1,25 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport(  {
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: 'reportdococument@gmail.com', // generated ethereal user
-      pass: 'sfhyoiqoejavngew', // generated ethereal password
-    }
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", // Server de Microsoft: smtp.live.com: 587
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER, // generated ethereal user  (reportdocument@hotmail.com)
+    pass: process.env.MAIL_PASSWORD, // generated ethereal password  (rwlcfvalfxpzavfs)
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+transporter
+  .verify()
+  .then(() => {
+    console.log("Ready for send emails");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-  transporter.verify().then(() =>{
-    console.log('Ready for send emails');
-  });
-
-  module.exports = transporter;
+module.exports = transporter;
--- app/controllers/registerUser-controller.js
+++ app/controllers/registerUser-controller.js
@@ -2,63 +2,57 @@ const db = require("../db/mysql");
 const bcrypt = require("bcryptjs");
 const transporter = require("../config/mailer");
 
-let register = async (req, res) => {
+let register = (req, res) => {
   let hashPass = bcrypt.hashSync(req.body.password, 8);
   req.body.password = hashPass;
 
-  try {
-    // send mail with defined transport object
-    await transporter.sendMail({
-      from: '"Reporte Documentos 👻" <reportdococument@gmail.com>', // sender address
-      to: "jhonattan1997@hotmail.com", // list of receivers
-      subject: "cliente de correo ✔", // Subject line
-      text: "Bienvenido a la pagina web de Documentos Perdidos", // plain text body
-      html: `
-    <table style="border: 1px solid #ccc">
-
-    <thead>
+  db.registerUser(req.body)
+    .then(async (result) => {
+      try {
+        // send mail with defined transport object
+        await transporter.sendMail({
+          from: '"Reporte De Documentos" <reportdococument@gmail.com>', // sender address
+          to: `${req.body.email}`, // list of receivers
+          subject: "Registro exitoso ✔", // Subject line
+          text: "Bienvenido a la pagina web de Documentos Perdidos", // plain text body
+          html: `
+        <table style="border: 1px solid #ccc">
+    
+        <thead>
+            <tr>
+                  <th style="border: 1px solid #ccc">Integrante 1</th>
+                  <th style="border: 1px solid #ccc">Integrante 2</th>
+                  <th style="border: 1px solid #ccc">Integrante 3</th>  
+            </tr>
+        </thead>
+    
         <tr>
-              <th style="border: 1px solid #ccc">Integrante 1</th>
-              <th style="border: 1px solid #ccc">Integrante 2</th>
-              <th style="border: 1px solid #ccc">Integrante 3</th>  
+    
+          <td style="border: 1px solid #ccc">Jhonattan</td>
+    
+          <td style="border: 1px solid #ccc">Vanessa</td>
+    
+          <td style="border: 1px solid #ccc">Juan Camilo</td>
+    
         </tr>
-    </thead>
-
-    <tr>
-
-      <td style="border: 1px solid #ccc">Jhonattan</td>
-
-      <td style="border: 1px solid #ccc">Vanessa</td>
-
-      <td style="border: 1px solid #ccc">Juan Camilo</td>
-
-    </tr>
-    </table>
-    `, // html body
-
-    });
-    return res.status(200).json({
-      status: "Successful registration",
-      reg: true
-    });
-  } catch (error) {
-    //emailStatus = error;
-    return res.status(400).json({ message: 'Sometring goes wrong' });
-  }
-
-
-
-  /**   db.registerUser(req.body)
-    .then((result) => {
+        </table>
+        `, // html body
+        });
+      } catch (error) {
+        //emailStatus = error;
+        return res.status(400).json({
+          message: "Something went wrong with the email",
+          error: error,
+        });
+      }
       return res.status(200).json({
         status: "Successful registration",
-        reg: true
+        reg: true,
       });
     })
     .catch((err) => {
       console.log(err);
     });
-    */
 };
 
 module.exports = {
