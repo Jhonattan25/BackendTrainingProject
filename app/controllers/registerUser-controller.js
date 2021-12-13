916df8747954cc97b99e3ef153b7f1b7275969e8
jcamilovd <jcvargasd@uqvirtual.edu.co>
2021-12-13 01:38:54 -0500

    remove document pdf the email

--- app/controllers/registerUser-controller.js
+++ app/controllers/registerUser-controller.js
@@ -57,7 +57,7 @@ let register = (req, res) => {
 
             attachments: [
               {
-                path: "./app/documents/Proyecto_web_GetIt.pdf",
+                path: "",
               },
             ],
           },
